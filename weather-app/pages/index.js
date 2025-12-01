81fb62b73b1c50d4090d51612fcbe579a7697d82
안혜연 <ahy020606@naver.com>
2025-12-01 20:27:24 +0900

    [style] 레이아웃용 module.css 기본 스타일 추가 #1

--- weather-app/pages/index.js
+++ weather-app/pages/index.js
@@ -6,6 +6,7 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Weather App</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
         <meta name="description" content="Weather App - 도시별 현재 날씨와 5일 예보를 확인할 수 있는 서비스" />
         <meta property="og:title" content="Weather App" />
         <meta property="og:description" content="도시별 현재 날씨와 5일 예보 정보를 확인하세요." />
