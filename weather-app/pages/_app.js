ffdc4169d8287117a5569bdf4312565419987504
안혜연 <ahy020606@naver.com>
2025-12-02 19:09:42 +0900

    [feat] Apollo Client 설정 및 전역 Provider 적용 #4

--- weather-app/pages/_app.js
+++ weather-app/pages/_app.js
@@ -1,11 +1,15 @@
 import '../styles/globals.css';
 import Layout from '../components/Layout/Layout';
+import { ApolloProvider } from '@apollo/client';
+import client from '../lib/apolloClient';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ApolloProvider client={client}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ApolloProvider>
   );
 }
 
